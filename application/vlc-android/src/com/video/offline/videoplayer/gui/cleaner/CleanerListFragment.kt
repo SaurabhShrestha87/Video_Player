deaf7e0f2a554d11d5e20673257bf35d5a3dcbf9
saurabh <shresthasaurabh86@gmail.com>
2024-08-03 17:58:03 +0530

    cleaner fragment changes

--- application/vlc-android/src/com/video/offline/videoplayer/gui/cleaner/CleanerListFragment.kt
+++ application/vlc-android/src/com/video/offline/videoplayer/gui/cleaner/CleanerListFragment.kt
@@ -100,13 +100,11 @@ import org.videolan.resources.KEY_FOLDER
 import org.videolan.resources.KEY_GROUP
 import org.videolan.resources.KEY_GROUPING
 import org.videolan.resources.KEY_GROUP_VIDEOS
-import org.videolan.resources.KEY_MEDIA_LAST_PLAYLIST
 import org.videolan.resources.KEY_VIDEOS_CARDS
 import org.videolan.resources.KEY_VIDEOS_COMPACT
 import org.videolan.resources.KEY_VIDEOS_LIST
 import org.videolan.resources.MOVIEPEDIA_ACTIVITY
 import org.videolan.resources.MOVIEPEDIA_MEDIA
-import org.videolan.resources.PLAYLIST_TYPE_VIDEO
 import org.videolan.resources.util.parcelable
 import org.videolan.resources.util.waitForML
 import org.videolan.tools.MultiSelectHelper
@@ -117,6 +115,8 @@ import org.videolan.tools.dp
 import org.videolan.tools.isStarted
 import org.videolan.tools.putSingle
 import org.videolan.tools.retrieveParent
+import org.videolan.tools.setGone
+import org.videolan.tools.setVisible
 import java.io.File
 
 
@@ -249,70 +249,18 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
 
     override fun onPrepareOptionsMenu(menu: Menu) {
         super.onPrepareOptionsMenu(menu)
-        menu.findItem(R.id.ml_menu_last_playlist).isVisible =
-            settings.contains(KEY_MEDIA_LAST_PLAYLIST)
-        menu.findItem(R.id.rename_group).isVisible = viewModel.group != null
-        menu.findItem(R.id.ungroup).isVisible = viewModel.group != null
+        menu.findItem(R.id.ml_menu_last_playlist).isVisible = false
+        menu.findItem(R.id.rename_group).isVisible = false
+        menu.findItem(R.id.ungroup).isVisible = false
         menu.findItem(R.id.ml_menu_sortby).isVisible = false
         // TODO: VISIBILITY
 //        menu.findItem(R.id.ml_menu_display_options).isVisible = parentFragment is VideoBrowserFragment
         menu.findItem(R.id.ml_menu_display_options).isVisible = false
-        if (requireActivity().isTalkbackIsEnabled()) menu.findItem(R.id.play_all).isVisible = true
+        if (requireActivity().isTalkbackIsEnabled()) menu.findItem(R.id.play_all).isVisible = false
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.ml_menu_last_playlist -> {
-                MediaUtils.loadlastPlaylist(activity, PLAYLIST_TYPE_VIDEO)
-            }
-
-            R.id.rename_group -> {
-                viewModel.group?.let { renameGroup(it) }
-            }
-
-            R.id.ungroup -> {
-                viewModel.group?.let {
-                    lifecycleScope.launch {
-                        if (requireActivity().showPinIfNeeded()) viewModel.ungroup(
-                            it
-                        )
-                    }
-                }
-            }
-
-            R.id.play_all -> {
-                onFabPlayClick(binding.root)
-            }
-
-            R.id.ml_menu_display_options -> {
-                //filter all sorts and keep only applicable ones
-                val sorts = arrayListOf(
-                    Medialibrary.SORT_ALPHA,
-                    Medialibrary.SORT_FILENAME,
-                    Medialibrary.SORT_ARTIST,
-                    Medialibrary.SORT_ALBUM,
-                    Medialibrary.SORT_DURATION,
-                    Medialibrary.SORT_RELEASEDATE,
-                    Medialibrary.SORT_LASTMODIFICATIONDATE,
-                    Medialibrary.SORT_FILESIZE,
-                    Medialibrary.NbMedia,
-                    Medialibrary.SORT_INSERTIONDATE
-                ).filter {
-                    viewModel.provider.canSortBy(it)
-                }
-                //Open the display settings Bottom sheet
-                DisplaySettingsDialog.newInstance(
-                    displayInCards = settings.getBoolean(KEY_VIDEOS_CARDS, true),
-                    onlyFavs = viewModel.provider.onlyFavorites,
-                    sorts = sorts,
-                    currentSort = viewModel.provider.sort,
-                    currentSortDesc = viewModel.provider.desc,
-                    videoGroup = settings.getString(KEY_GROUP_VIDEOS, GROUP_VIDEOS_NONE)
-                ).show(requireActivity().supportFragmentManager, "DisplaySettingsDialog")
-            }
-
-            else -> return super.onOptionsItemSelected(item)
-        }
         return true
     }
 
@@ -329,6 +277,7 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
         (activity as? AppCompatActivity)?.run {
             supportActionBar?.title = title
             invalidateOptionsMenu()
+            binding.cleanBtn.setGone()
         }
     }
 
@@ -430,13 +379,7 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
         )
     }
 
-    override fun getTitle() = when (viewModel.groupingType) {
-        VideoGroupingType.NONE -> viewModel.folder?.displayTitle ?: viewModel.group?.displayTitle
-        ?: getString(R.string.videos)
-
-        VideoGroupingType.FOLDER -> getString(R.string.videos_folders_title)
-        VideoGroupingType.NAME -> getString(R.string.videos_groups_title)
-    }
+    override fun getTitle() = "Cleaner"
 
     @Suppress("UNCHECKED_CAST")
     override fun getMultiHelper(): MultiSelectHelper<VideosViewModel>? =
@@ -586,6 +529,12 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
 
         }
         setFabPlayVisibility(!empty)
+        binding.cleanBtn.setOnClickListener {
+            if (multiSelectHelper.getSelection().isEmpty()) {
+                return@setOnClickListener
+            }
+            removeItems(multiSelectHelper.getSelection())
+        }
     }
 
     override fun onRefresh() {
@@ -1069,7 +1018,13 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
     private fun onLongClick(position: Int) {
         if (actionMode == null && inSearchMode()) UiTools.setKeyboardVisibility(binding.root, false)
         multiSelectHelper.toggleSelection(position, true)
-        if (actionMode == null) startActionMode() else invalidateActionMode()
+        if (actionMode == null) {
+            startActionMode()
+            binding.cleanBtn.setVisible()
+        } else {
+            invalidateActionMode()
+            binding.cleanBtn.setGone()
+        }
     }
 
     private fun onClick(position: Int, item: MediaLibraryItem) {
@@ -1078,6 +1033,7 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
                 if (actionMode != null) {
                     multiSelectHelper.toggleSelection(position)
                     invalidateActionMode()
+                    binding.cleanBtn.setGone()
                 } else {
                     viewModel.playVideo(activity, item, position)
                 }
@@ -1088,6 +1044,7 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
                 if (actionMode != null) {
                     multiSelectHelper.toggleSelection(position)
                     invalidateActionMode()
+                    binding.cleanBtn.setGone()
                 } else activity?.open(item)
             }
 
@@ -1095,6 +1052,7 @@ class CleanerListFragment : MediaBrowserFragment<VideosViewModel>(),
                 actionMode != null -> {
                     multiSelectHelper.toggleSelection(position)
                     invalidateActionMode()
+                    binding.cleanBtn.setGone()
                 }
 
                 item.presentCount == 0 -> UiTools.snackerMissing(requireActivity())
